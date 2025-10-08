3d7ee8a2578d386bce6f37fa9c0654e0406d53cc
ZaLa0987654321 <gl4444eb@gmail.com>
2025-10-08 17:58:15 +0300

    - mods -

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from git import Repo
 from os import system
+import os
 
 repo_url = "https://github.com/ZaLa0987654321/HaplinOnlineAutoModLoader.git"
 
@@ -8,4 +9,5 @@ repo = Repo.clone_from(repo_url, "Cloned")
 
 tree = repo.head.commit.tree
 
-print(tree["README.md"])
+for f in tree["mods"]:
+	print(os.path.splitext(f.path))
